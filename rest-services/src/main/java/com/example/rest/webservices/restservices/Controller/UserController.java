46f951baeed79d17e6cc197655bd592206434447
TanmayTapar <tanmaytapar@gmail.com>
2018-02-24 14:08:32 -0500

    Support for Content Negotiation, added support for xml response

--- rest-services/src/main/java/com/example/rest/webservices/restservices/Controller/UserController.java
+++ rest-services/src/main/java/com/example/rest/webservices/restservices/Controller/UserController.java
@@ -4,9 +4,12 @@ import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.xml.ws.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,13 +34,16 @@ public class UserController {
 	}
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id){
+	public Resource<User> retrieveUser(@PathVariable int id){
 		User user =userService.findOne(id);
 		if(user==null){
 			throw new UserNotFoundException("id->" + id);
 		}
 		else{
-			return user;
+			Resource<User> resource= new Resource<User>(user);
+			ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+			resource.add(linkTo.withRel("all-users"));
+			return resource;
 		}
 		
 	}
--- rest-services/src/main/java/com/example/rest/webservices/restservices/RestServicesApplication.java
+++ rest-services/src/main/java/com/example/rest/webservices/restservices/RestServicesApplication.java
@@ -1,7 +1,12 @@
 package com.example.rest.webservices.restservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestServicesApplication {
@@ -9,4 +14,5 @@ public class RestServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServicesApplication.class, args);
 	}
+	
 }
