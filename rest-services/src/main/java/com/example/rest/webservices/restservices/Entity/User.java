1b50d01bee2f4923b29965ae6b064a07e59431a8
TanmayTapar <tanmaytapar@gmail.com>
2018-02-24 15:34:29 -0500

    Added Swagger for Documentation

--- rest-services/src/main/java/com/example/rest/webservices/restservices/Entity/User.java
+++ rest-services/src/main/java/com/example/rest/webservices/restservices/Entity/User.java
@@ -5,12 +5,19 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+@ApiModel(description= "All Info about User")
 public class User {
 	
 	private Integer id;
+	
 	@Size(min=2,message="Name should have atleast 2 characters")
+	@ApiModelProperty(notes="Should be atleast 2 char")	
 	private String name;
+	
 	@Past()
+	@ApiModelProperty(notes="Birthdate should be in past")
 	private Date birthDate;
 	
 	public User(){
